42b8123b33e2c4ed3bff062d5348e9920f3da2f1
gartemy <artemganzhaa@gmail.com>
2023-05-02 16:13:26 +0500

    Занятие 27.04.2023

---
+++ platforms.py
@@ -0,0 +1,46 @@
+import pygame
+from random import randint
+
+pygame.init()
+
+WIDTH = 1920
+HEIGHT = 1200
+
+window = pygame.display.set_mode([WIDTH, HEIGHT], pygame.FULLSCREEN)
+pygame.display.set_caption('Контр-страйк 2 (оригинал) бета-тест 2')
+
+FPS = 60
+clock = pygame.time.Clock()
+
+man_jump = pygame.image.load('man_jump.png')
+man_stand = pygame.image.load('man_stand.png')
+platform = pygame.image.load('кирпич шоколадка small.png')
+
+map = []
+
+for i in range(18):
+    stroka = ''
+    for j in range(31):
+        stroka += str(randint(0, 1))
+    map.append(stroka)
+
+while True:
+    window.fill(pygame.Color('pink'))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    platforms = []
+
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == '1':
+                platform_rect = platform.get_rect()
+                platform_rect.x = platform_rect.width * j
+                platform_rect.y = platform_rect.height * i
+                platforms.append(platform_rect)
+                window.blit(platform, platform_rect)
+
+    pygame.display.update()
+    clock.tick(FPS)
